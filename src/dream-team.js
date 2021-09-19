8a21779dbdc3545df13453a70085039a2de81f38
K1ndum <57758788+K1ndum@users.noreply.github.com>
2021-09-19 22:28:49 +0300

    refactor
    Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(arr) {
+  if (!arr) {
+    return false;
+  }
+  let array = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof(arr[i]) == "string") {
+      array.push((arr[i].match(/\w/))[0].toLocaleUpperCase());  
+    }
+  }
+  return array.sort().join('');
 }
