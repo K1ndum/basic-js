44c8a8e77c365a590a581c7790c61f3e235b3004
K1ndum <57758788+K1ndum@users.noreply.github.com>
2021-09-22 18:34:45 +0300

    feat
    implemented vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,80 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
+  constructor (value) {
+    this.value = value;
+  }
+
   encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let str = arguments[0];
+    let keyword = arguments[1];
+    if (str == undefined || keyword== undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+    let lang = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let arr = lang.toUpperCase().split('');
+    let strArr = str.toUpperCase().split('');
+    let keywordArr = keyword.toUpperCase().split('');
+    let count = 0;
+    let codeWordArr = [];
+    for (let i = 0; i < strArr.length; i++) {
+      if (arr.indexOf(strArr[i]) !== -1) {
+        if (count >= keywordArr.length) {
+          count = 0;
+        }
+        let indexKey = arr.indexOf(keywordArr[count]);
+        count++;
+        let indexStr = arr.indexOf(strArr[i]);
+        let code = indexKey + indexStr;
+        if (code >= 26) {
+          code = code - 26;
+        }
+        codeWordArr.push(arr[code]);
+      } else {
+        codeWordArr.push(strArr[i]);
+      }
+    }
+    if (this.value == true || this.value == undefined) {
+      return codeWordArr.join('');
+    } else {
+      return codeWordArr.reverse().join('');
+    }
+
+
   }
   decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let str = arguments[0];
+    let keyword = arguments[1];
+    if (str == undefined || keyword == undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+    let lang = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let arr = lang.toUpperCase().split('');
+    let strArr = str.toUpperCase().split('');
+    let keywordArr = keyword.toUpperCase().split('');
+    let count = 0;
+    let codeWordArr = [];
+    for (let i = 0; i < strArr.length; i++) {
+      if (arr.indexOf(strArr[i]) !== -1) {
+        if (count >= keywordArr.length) {
+          count = 0;
+        }
+        let indexKey = arr.indexOf(keywordArr[count]);
+        count++;
+        let indexStr = arr.indexOf(strArr[i]);
+        let code = indexStr - indexKey;
+        if (code < 0) {
+          code = code + 26;
+        }
+        codeWordArr.push(arr[code]);
+      } else {
+        codeWordArr.push(strArr[i]);
+      }
+    }
+    if (this.value == true || this.value == undefined) {
+      return codeWordArr.join('');
+    } else {
+      return codeWordArr.reverse().join('');
+    }
   }
 }
