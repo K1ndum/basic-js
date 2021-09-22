599793e4e74e93493ca36d3d63d275f048b183b7
K1ndum <57758788+K1ndum@users.noreply.github.com>
2021-09-22 14:34:13 +0300

    Update extended-repeater.js
    Implemented extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  if (options.repeatTimes == undefined) {
+    options.repeatTimes = 1;
+  }
+  let arr = [];
+  for (let i = 0; i < options.repeatTimes; i++) {
+    if (str == true) {
+      arr.push('true');
+    } else if ((str == null)) {
+      arr.push('null');
+    } else {
+      arr.push(str);
+    }
+    if(options.addition !== undefined) {
+      arr[i] += options.addition
+      if (options.additionRepeatTimes !== undefined) {
+        for (let j = 1; j < options.additionRepeatTimes; j++) {
+          if (options.additionSeparator !== undefined) {
+            arr[i] += options.additionSeparator + options.addition;
+          } else {
+            arr[i] += '|' + options.addition;
+          }
+        }
+      }
+    }
+  }
+  console.log(arr)
+  if (options.separator == undefined) {
+    return arr.join('+');
+  } else {
+    return arr.join(`${options.separator}`);
+  }
 }
+
